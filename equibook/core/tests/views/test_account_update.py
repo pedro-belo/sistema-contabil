8839a3a511150a9c0b0ee2b9ea2abe7e5efac16c
Pedro Belo <pn999717@gmail.com>
2024-01-12 22:52:25 -0300

    update AccountUpdateViewTests

--- equibook/core/tests/views/test_account_update.py
+++ equibook/core/tests/views/test_account_update.py
@@ -5,63 +5,35 @@ from equibook.core import facade
 class AccountUpdateViewTests(base.TestCase):
     def setUp(self) -> None:
         self.user = base.create_default_user()
+        self.account = facade.Account.objects.get_asset(self.user)
+        self.url_update = base.reverse("core:account-update", args=[self.account.id])
+        self.form_data = {"name": f"{self.account.name} updated"}
 
     def test_get_authenticated(self):
-        account = facade.Account.objects.get_asset(self.user)
         self.client.force_login(self.user)
-        response = self.client.get(
-            base.reverse("core:account-update", args=[account.id])
-        )
+        response = self.client.get(self.url_update)
         self.assertEqual(response.status_code, 200)
 
     def test_get_unauthenticated(self):
-        account = facade.Account.objects.get_asset(self.user)
-
-        expected_url = (
-            base.reverse("users:login")
-            + "?next="
-            + base.reverse("core:account-update", args=[account.id])
-        )
-
-        response = self.client.get(
-            base.reverse("core:account-update", args=[account.id])
-        )
-
+        response = self.client.get(self.url_update)
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, expected_url)
+        self.assertEqual(response.url, base.url_login_next(self.url_update))
 
     def test_update_authenticated(self):
-        account = facade.Account.objects.get_asset(self.user)
-
         self.client.force_login(self.user)
 
-        data = {"name": f"{account.name} updated"}
-
-        response = self.client.post(
-            base.reverse("core:account-update", args=[account.id]),
-            data=data,
-        )
+        response = self.client.post(self.url_update, data=self.form_data)
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(
-            response.url, base.reverse("core:account-detail", args=[account.id])
+            response.url,
+            base.reverse("core:account-detail", args=[self.account.id]),
         )
 
         account = facade.Account.objects.get_asset(self.user)
-        self.assertEqual(account.name, data["name"])
+        self.assertEqual(account.name, self.form_data["name"])
 
     def test_update_unauthenticated(self):
-        account = facade.Account.objects.get_asset(self.user)
-
-        expected_url = (
-            base.reverse("users:login")
-            + "?next="
-            + base.reverse("core:account-update", args=[account.id])
-        )
-
-        data = {"name": f"{account.name} updated"}
-
-        response = self.client.post(base.reverse("core:account-update", args=[account.id]), data=data)
-
+        response = self.client.post(self.url_update, data=self.form_data)
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, expected_url)
+        self.assertEqual(response.url, base.url_login_next(self.url_update))
