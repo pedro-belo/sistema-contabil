bf2d858189c5e16548c96dc95e6eecd20711e593
Pedro Belo <pn999717@gmail.com>
2024-01-12 22:46:05 -0300

    update AccountDetailViewTests

--- equibook/core/tests/views/test_account_detail.py
+++ equibook/core/tests/views/test_account_detail.py
@@ -5,27 +5,15 @@ from equibook.core import facade
 class AccountDetailViewTests(base.TestCase):
     def setUp(self) -> None:
         self.user = base.create_default_user()
+        self.account = facade.Account.objects.get_asset(self.user)
+        self.url_detail = base.reverse("core:account-detail", args=[self.account.id])
 
     def test_get_authenticated(self):
-        account = facade.Account.objects.get_asset(self.user)
         self.client.force_login(self.user)
-        response = self.client.get(
-            base.reverse("core:account-detail", args=[account.id])
-        )
+        response = self.client.get(self.url_detail)
         self.assertEqual(response.status_code, 200)
 
     def test_get_unauthenticated(self):
-        account = facade.Account.objects.get_asset(self.user)
-
-        expected_url = (
-            base.reverse("users:login")
-            + "?next="
-            + base.reverse("core:account-detail", args=[account.id])
-        )
-
-        response = self.client.get(
-            base.reverse("core:account-detail", args=[account.id])
-        )
-
+        response = self.client.get(self.url_detail)
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, expected_url)
+        self.assertEqual(response.url, base.url_login_next(self.url_detail))
