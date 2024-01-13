9994286a94a82e822d382f6d95775c17d2dadf4c
Pedro Belo <pn999717@gmail.com>
2024-01-12 22:39:42 -0300

    update AccountCreateViewTests

--- equibook/core/tests/views/test_account_create.py
+++ equibook/core/tests/views/test_account_create.py
@@ -2,75 +2,44 @@ from equibook.core.tests import base
 from equibook.core import facade
 
 
-class AccountUpdateViewTests(base.TestCase):
+class AccountCreateViewTests(base.TestCase):
     def setUp(self) -> None:
         self.user = base.create_default_user()
+        self.account = facade.Account.objects.get_equity(self.user)
+        self.url_create = base.reverse("core:account-create", args=[self.account.id])
+        self.form_data = {"name": f"shinzou wo sasageyo"}
 
     def test_get_authenticated(self):
-        parent_account = facade.Account.objects.get_equity(self.user)
-
         self.client.force_login(self.user)
-
-        response = self.client.get(
-            base.reverse("core:account-create", args=[parent_account.id])
-        )
+        response = self.client.get(self.url_create)
         self.assertEqual(response.status_code, 200)
 
     def test_get_unauthenticated(self):
-        parent_account = facade.Account.objects.get_equity(self.user)
-
-        expected_url = (
-            base.reverse("users:login")
-            + "?next="
-            + base.reverse("core:account-create", args=[parent_account.id])
-        )
-
-        response = self.client.get(
-            base.reverse("core:account-create", args=[parent_account.id])
-        )
-
+        response = self.client.get(self.url_create)
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, expected_url)
+        self.assertEqual(response.url, base.url_login_next(self.url_create))
 
     def test_create_authenticated(self):
         num_accounts = facade.Account.objects.count()
-        parent_account = facade.Account.objects.get_asset(self.user)
-
         self.client.force_login(self.user)
 
-        data = {"name": f"shinzou wo sasageyo"}
+        response = self.client.post(self.url_create, data=self.form_data)
 
-        response = self.client.post(
-            base.reverse("core:account-create", args=[parent_account.id]),
-            data=data,
-        )
-
-        account = facade.Account.objects.last()
+        created = facade.Account.objects.last()
         self.assertEqual(response.status_code, 302)
         self.assertEqual(
-            response.url, base.reverse("core:account-detail", args=[account.id])
+            response.url,
+            base.reverse("core:account-detail", args=[created.id]),
         )
 
-        self.assertEqual(account.name, data["name"])
+        self.assertEqual(created.name, self.form_data["name"])
         self.assertEqual(facade.Account.objects.count(), num_accounts + 1)
 
     def test_create_unauthenticated(self):
         num_accounts = facade.Account.objects.count()
-        parent_account = facade.Account.objects.get_asset(self.user)
 
-        expected_url = (
-            base.reverse("users:login")
-            + "?next="
-            + base.reverse("core:account-create", args=[parent_account.id])
-        )
-
-        data = {"name": f"shinzou wo sasageyo"}
-
-        response = self.client.post(
-            base.reverse("core:account-create", args=[parent_account.id]),
-            data=data,
-        )
+        response = self.client.post(self.url_create, data=self.form_data)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, expected_url)
+        self.assertEqual(response.url, base.url_login_next(self.url_create))
         self.assertEqual(facade.Account.objects.count(), num_accounts)
