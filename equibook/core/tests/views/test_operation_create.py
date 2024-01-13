40928e856a07182e6bf0d7cbf70f2ccd40ae83f7
Pedro Belo <pn999717@gmail.com>
2024-01-13 15:25:58 -0300

    update OperationCreateViewTestCase

--- equibook/core/tests/views/test_operation_create.py
+++ equibook/core/tests/views/test_operation_create.py
@@ -3,7 +3,7 @@ from equibook.core import facade
 from decimal import Decimal
 
 
-class OperationCreateViewTests(base.TestCase):
+class OperationCreateViewTestCase(base.TestCase):
     def setUp(self) -> None:
         self.user = base.create_default_user()
         self.period = base.create_period(self.user)
@@ -24,40 +24,28 @@ class OperationCreateViewTests(base.TestCase):
                 "account_period": self.period,
             },
         )
+        self.url_create = base.reverse(
+            "core:operation-create", args=[self.transaction.id]
+        )
 
     def test_get_authenticated(self):
         self.client.force_login(self.user)
-
-        response = self.client.get(
-            base.reverse("core:operation-create", args=[self.transaction.id])
-        )
+        response = self.client.get(self.url_create)
         self.assertEqual(response.status_code, 200)
 
     def test_get_unauthenticated(self):
-        expected_url = (
-            base.reverse("users:login")
-            + "?next="
-            + base.reverse("core:operation-create", args=[self.transaction.id])
-        )
-
-        response = self.client.get(
-            base.reverse("core:operation-create", args=[self.transaction.id])
-        )
-
+        response = self.client.get(self.url_create)
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, expected_url)
+        self.assertEqual(response.url, base.url_login_next(self.url_create))
 
     def test_create_authenticated(self):
         self.client.force_login(self.user)
 
         self.assertEqual(facade.Operation.objects.count(), 0)
 
-        response = self.client.post(
-            base.reverse("core:operation-create", args=[self.transaction.id]),
-            data=self.form_data,
-        )
-        created = facade.Operation.objects.last()
+        response = self.client.post(self.url_create, data=self.form_data)
 
+        created = facade.Operation.objects.last()
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, base.reverse("core:transaction-list"))
         self.assertEqual(created.account_id, self.account.id)
@@ -71,7 +59,6 @@ class OperationCreateViewTests(base.TestCase):
         self.assertEqual(facade.Operation.objects.count(), 1)
 
     def test_create_closed_period(self):
-
         self.period.status = facade.AccountingPeriod.Status.CLOSED
         self.period.save()
 
@@ -81,28 +68,16 @@ class OperationCreateViewTests(base.TestCase):
 
         self.assertEqual(facade.Operation.objects.count(), 0)
 
-        response = self.client.post(
-            base.reverse("core:operation-create", args=[self.transaction.id]),
-            data=self.form_data,
-        )
+        response = self.client.post(self.url_create, data=self.form_data)
 
         self.assertEqual(response.status_code, 404)
         self.assertEqual(facade.Operation.objects.count(), 0)
 
     def test_create_unauthenticated(self):
-        expected_url = (
-            base.reverse("users:login")
-            + "?next="
-            + base.reverse("core:operation-create", args=[self.transaction.id])
-        )
-
         self.assertEqual(facade.Operation.objects.count(), 0)
 
-        response = self.client.post(
-            base.reverse("core:operation-create", args=[self.transaction.id]),
-            data=self.form_data,
-        )        
+        response = self.client.post(self.url_create, data=self.form_data)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, expected_url)
+        self.assertEqual(response.url, base.url_login_next(self.url_create))
         self.assertEqual(facade.Operation.objects.count(), 0)
