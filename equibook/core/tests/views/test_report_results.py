a2d05c199c2953af50e8e8f7828eafd6df2cf099
Pedro Belo <pn999717@gmail.com>
2024-01-13 10:53:51 -0300

    update ResultAccountTestCase

--- equibook/core/tests/views/test_report_results.py
+++ equibook/core/tests/views/test_report_results.py
@@ -1,38 +1,25 @@
-from django.urls import reverse
-from django.test import TestCase
+from equibook.core.tests import base
 from equibook.core import facade
-from equibook.users.models import User
-from model_bakery import baker
 
 
-class ResultsViewTest(TestCase):
+class ResultAccountTestCase(base.TestCase):
     def setUp(self):
-        self.user = baker.make(User)
-        facade.user_setup(
-            self.user,
-            form_data={
-                "entity": "Entity Name",
-                "base_currency": facade.Currency.BRL,
-            },
-        )
+        self.user = base.create_default_user()
+        self.url_ac_result = base.reverse("core:result-accounts")
 
     def test_get_unauthenticated(self):
-        url = reverse("core:result-accounts")
-        login_url = reverse("users:login")
-
-        response = self.client.get(url)
+        response = self.client.get(self.url_ac_result)
         self.assertEqual(response.status_code, 302)
-
-        self.assertIn(login_url, response.url)
+        self.assertIn(base.reverse("users:login"), response.url)
 
     def test_get_authenticated(self):
         self.client.force_login(self.user)
-        response = self.client.get(reverse("core:result-accounts"))
+        response = self.client.get(self.url_ac_result)
         self.assertEqual(response.status_code, 200)
 
     def test_context(self):
         self.client.force_login(self.user)
-        response = self.client.get(reverse("core:result-accounts"))
+        response = self.client.get(self.url_ac_result)
         self.assertEqual(
             response.context_data["revenue"],
             facade.Account.objects.get_revenue(self.user),
