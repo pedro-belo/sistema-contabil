8b452608a4fa23a3661bec954170c792ee70b0c8
Pedro Belo <pn999717@gmail.com>
2024-01-13 10:59:08 -0300

    update BalanceAccountsViewTest

--- equibook/core/tests/views/test_report_overview.py
+++ equibook/core/tests/views/test_report_overview.py
@@ -1,45 +1,37 @@
-from django.urls import reverse
-from django.test import TestCase
+from equibook.core.tests import base
 from equibook.core import facade
-from equibook.users.models import User
-from model_bakery import baker
 
 
-class BalanceAccountsViewTest(TestCase):
+class BalanceAccountsViewTest(base.TestCase):
     def setUp(self):
-        self.user = baker.make(User)
-        facade.user_setup(
-            self.user,
-            form_data={
-                "entity": "Entity Name",
-                "base_currency": facade.Currency.BRL,
-            },
-        )
+        self.user = base.create_default_user()
+        self.url_ac_balance = base.reverse("core:balance-accounts")
 
     def test_get_unauthenticated(self):
-        url = reverse("core:balance-accounts")
-        login_url = reverse("users:login")
-
+        url = self.url_ac_balance
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
-
-        self.assertIn(login_url, response.url)
+        self.assertEqual(response.url, base.url_login_next(self.url_ac_balance))
 
     def test_get_authenticated(self):
         self.client.force_login(self.user)
-        response = self.client.get(reverse("core:balance-accounts"))
+        response = self.client.get(self.url_ac_balance)
         self.assertEqual(response.status_code, 200)
 
     def test_context(self):
         self.client.force_login(self.user)
-        response = self.client.get(reverse("core:balance-accounts"))
+        response = self.client.get(self.url_ac_balance)
+
         self.assertEqual(
-            response.context_data["asset"], facade.Account.objects.get_asset(self.user)
+            response.context_data["asset"],
+            facade.Account.objects.get_asset(self.user),
         )
+
         self.assertEqual(
             response.context_data["equity"],
             facade.Account.objects.get_equity(self.user),
         )
+
         self.assertEqual(
             response.context_data["liability"],
             facade.Account.objects.get_liability(self.user),
