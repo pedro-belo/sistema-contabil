60c400e0ce1b03e891c8b6b09ca07bdc40b6f0ef
Pedro Belo <pn999717@gmail.com>
2024-01-12 23:00:03 -0300

    update TransactionDetailViewTests

--- equibook/core/tests/views/test_transaction_detail.py
+++ equibook/core/tests/views/test_transaction_detail.py
@@ -1,5 +1,4 @@
 from equibook.core.tests import base
-from equibook.core import facade
 
 
 class TransactionDetailViewTests(base.TestCase):
@@ -13,23 +12,16 @@ class TransactionDetailViewTests(base.TestCase):
             value=100,
         )
 
+        self.url_detail = base.reverse(
+            "core:transaction-detail", args=[self.transaction.id]
+        )
+
     def test_get_authenticated(self):
         self.client.force_login(self.user)
-        response = self.client.get(
-            base.reverse("core:transaction-detail", args=[self.transaction.id])
-        )
+        response = self.client.get(self.url_detail)
         self.assertEqual(response.status_code, 200)
 
     def test_get_unauthenticated(self):
-        expected_url = (
-            base.reverse("users:login")
-            + "?next="
-            + base.reverse("core:transaction-detail", args=[self.transaction.id])
-        )
-
-        response = self.client.get(
-            base.reverse("core:transaction-detail", args=[self.transaction.id])
-        )
-
+        response = self.client.get(self.url_detail)
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, expected_url)
+        self.assertEqual(response.url, base.url_login_next(self.url_detail))
