9c799ce04cf6988ae7824dfd26611b5f0deeccab
Pedro Belo <pn999717@gmail.com>
2024-01-12 22:58:22 -0300

    update TransactionListViewTests

--- equibook/core/tests/views/test_transaction_list.py
+++ equibook/core/tests/views/test_transaction_list.py
@@ -5,22 +5,16 @@ from equibook.core import facade
 class TransactionListViewTests(base.TestCase):
     def setUp(self) -> None:
         self.user = base.create_default_user()
+        self.url_list = base.reverse("core:transaction-list")
 
     def test_get_unauthenticated(self):
-        expected_url = (
-            base.reverse("users:login")
-            + "?next="
-            + base.reverse("core:transaction-list")
-        )
-
-        response = self.client.get(base.reverse("core:transaction-list"))
-
+        response = self.client.get(self.url_list)
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, expected_url)
+        self.assertEqual(response.url, base.url_login_next(self.url_list))
 
     def test_get_authenticated_ne_period_ne_transaction(self):
         self.client.force_login(self.user)
-        response = self.client.get(base.reverse("core:transaction-list"))
+        response = self.client.get(self.url_list)
         self.assertEqual(response.status_code, 200)
 
     def test_get_authenticated_e_period_ne_transaction(self):
@@ -31,14 +25,16 @@ class TransactionListViewTests(base.TestCase):
             period.status = status
             period.save()
 
-            response = self.client.get(base.reverse("core:transaction-list"))
+            response = self.client.get(self.url_list)
             self.assertEqual(response.status_code, 200)
 
     def test_get_authenticated_e_period_e_transaction(self):
         period = base.create_period(self.user)
 
         _, _, account = base.create_children_accounts(self.user)
-        _ = base.create_credts_and_debits(account=account, account_period=period, value=120)
+        _ = base.create_credts_and_debits(
+            account=account, account_period=period, value=120
+        )
 
         self.client.force_login(self.user)
 
@@ -46,5 +42,5 @@ class TransactionListViewTests(base.TestCase):
             period.status = status
             period.save()
 
-            response = self.client.get(base.reverse("core:transaction-list"))
+            response = self.client.get(self.url_list)
             self.assertEqual(response.status_code, 200)
