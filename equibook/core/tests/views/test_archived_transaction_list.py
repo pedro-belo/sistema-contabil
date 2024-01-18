8ea1b0ef38d3cc41e6e188b769f00afe82cdf982
Pedro Belo <pn999717@gmail.com>
2024-01-18 11:49:37 -0300

    ArchivedTransactionListViewTestCase

--- equibook/core/tests/views/test_archived_transaction_list.py
+++ equibook/core/tests/views/test_archived_transaction_list.py
@@ -2,7 +2,7 @@ from equibook.core.tests import base
 from equibook.core import facade
 
 
-class ArchivedTransactionListViewTests(base.TestCase):
+class ArchivedTransactionListViewTestCase(base.TestCase):
     def setUp(self) -> None:
         self.user = base.create_default_user()
         self.url_archived = base.reverse("core:transaction-list-archived")
@@ -12,12 +12,12 @@ class ArchivedTransactionListViewTests(base.TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, base.url_login_next(self.url_archived))
 
-    def test_get_authenticated_ne_period_ne_transaction(self):
+    def test_get_authenticated_ne_period(self):
         self.client.force_login(self.user)
         response = self.client.get(self.url_archived)
         self.assertEqual(response.status_code, 200)
 
-    def test_get_authenticated_e_period_ne_transaction(self):
+    def test_get_authenticated_ne_transaction(self):
         period = base.create_period(self.user)
         self.client.force_login(self.user)
 
@@ -28,12 +28,16 @@ class ArchivedTransactionListViewTests(base.TestCase):
             response = self.client.get(self.url_archived)
             self.assertEqual(response.status_code, 200)
 
-    def test_get_authenticated_e_period_e_transaction(self):
+    def test_get_authenticated(self):
         period = base.create_period(self.user)
 
-        _, _, account = base.create_children_accounts(self.user)
-        _ = base.create_credts_and_debits(
-            account=account, account_period=period, value=120
+        _, _, account = base.create_children_accounts(
+            user=self.user,
+            root=facade.Account.objects.get_asset(self.user),
+        )
+
+        self.transaction = base.create_debit_and_credit(
+            period=period, value=10, debit=account
         )
 
         self.client.force_login(self.user)
