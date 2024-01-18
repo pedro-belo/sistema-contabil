9caaf101b23c872b54ae9c64010e6c22c709c69b
Pedro Belo <pn999717@gmail.com>
2024-01-18 09:41:18 -0300

    AccountModelTestCase

--- equibook/core/tests/models/test_account.py
+++ equibook/core/tests/models/test_account.py
@@ -5,9 +5,12 @@ from equibook.core.tests import base
 class AccountModelTestCase(base.TestCase):
     def setUp(self) -> None:
         self.user = base.create_default_user()
+        self.asset = facade.Account.objects.get_asset(self.user)
+        self.liability = facade.Account.objects.get_liability(self.user)
 
-    def test_can_remove_root_accounts(self):
-        root_accounts = [
+    def test_root_accounts(self):
+        queryset = facade.Account.objects.filter(user=self.user, parent=None)
+        accounts = [
             facade.Account.objects.get_asset(self.user),
             facade.Account.objects.get_result(self.user),
             facade.Account.objects.get_liability(self.user),
@@ -16,115 +19,100 @@ class AccountModelTestCase(base.TestCase):
             facade.Account.objects.get_expense(self.user),
         ]
 
+        self.assertEqual(len(accounts), queryset.count())
+
+        for account in queryset:
+            self.assertTrue(queryset.contains(account))
+
+    def test_can_remove_root_accounts(self):
+        root_accounts = facade.Account.objects.filter(user=self.user, parent=None)
         for account in root_accounts:
             self.assertEqual(account.can_remove(), False)
 
     def test_can_remove_parent_account(self):
-        """
-        ACC2 é "pai" da conta ACC3
-        """
-        _, account, _ = base.create_children_accounts(self.user)
+        _, account, _ = base.create_children_accounts(self.user, root=self.asset)
         self.assertEqual(account.can_remove(), False)
 
     def test_can_delete_if_the_account_has_operation(self):
-        _, _, account = base.create_children_accounts(self.user)
+        _, _, account = base.create_children_accounts(self.user, root=self.asset)
         base.baker.make(facade.Operation, account=account)
         self.assertEqual(account.can_remove(), False)
 
-    def test_can_delete_when_account_is_a_leaf(self):
-        _, _, leaf = base.create_children_accounts(self.user)
+    def test_can_delete_if_account_is_leaf(self):
+        _, _, leaf = base.create_children_accounts(self.user, root=self.asset)
         self.assertEqual(leaf.can_remove(), True)
 
     def test_account_pretty_name(self):
-        acc1, acc2, acc3 = base.create_children_accounts(self.user)
+        acc1, acc2, acc3 = base.create_children_accounts(self.user, root=self.asset)
         self.assertEqual(str(acc3), f"{acc1.name} • {acc2.name} • {acc3.name}")
 
     def test_get_path(self):
-        acc1, acc2, acc3 = base.create_children_accounts(self.user)
+        acc1, acc2, acc3 = base.create_children_accounts(self.user, root=self.asset)
         path = acc3.get_path(self_include=True, reverse=True)
         self.assertEqual(acc1.id, path[0].id)
         self.assertEqual(acc2.id, path[1].id)
         self.assertEqual(acc3.id, path[2].id)
 
+    # TODO
     def test_get_recursive_childrens(self):
         ...
-        # TODO
 
     def test_individual_balance_zero(self):
         period = base.create_period(self.user)
-        _, acc, _ = base.create_children_accounts(self.user)
+        _, debit, credit = base.create_children_accounts(self.user, root=self.asset)
 
-        base.create_credts_and_debits(
-            account=acc,
-            account_period=period,
-            value=20,
+        base.create_debit_and_credit(
+            period=period, debit=debit, credit=credit, value=base.Decimal("20")
         )
-        self.assertEqual(acc.get_individual_balance(), 0)
+
+        base.create_debit_and_credit(
+            period=period, debit=credit, credit=debit, value=base.Decimal("20")
+        )
+
+        self.assertEqual(debit.get_individual_balance(), 0)
+        self.assertEqual(credit.get_individual_balance(), 0)
 
     def test_individual_balance_debit(self):
         period = base.create_period(self.user)
-        _, acc, _ = base.create_children_accounts(self.user)
+        _, debit, credit = base.create_children_accounts(self.user, root=self.asset)
 
-        base.create_credts_and_debits(
-            account=acc, account_period=period, value=20, credit=False
+        base.create_debit_and_credit(
+            period=period, debit=debit, credit=credit, value=base.Decimal("20")
         )
-        self.assertEqual(acc.get_individual_balance(), 20)
+
+        self.assertEqual(debit.get_individual_balance(), 20)
 
     def test_individual_balance_credit(self):
         period = base.create_period(self.user)
-        _, acc, _ = base.create_children_accounts(self.user)
+        _, debit, credit = base.create_children_accounts(self.user, root=self.liability)
 
-        base.create_credts_and_debits(
-            account=acc, account_period=period, value=20, debit=False
+        base.create_debit_and_credit(
+            period=period, value=base.Decimal("20"), debit=debit, credit=credit
         )
-        self.assertEqual(acc.get_individual_balance(), -20)
+
+        self.assertEqual(credit.get_individual_balance(), 20)
 
     def test_total_account_balance_zero(self):
         period = base.create_period(self.user)
-        _, acc1, acc2 = base.create_children_accounts(self.user)
+        _, debit, credit = base.create_children_accounts(self.user, root=self.asset)
 
-        base.create_credts_and_debits(account=acc1, account_period=period, value=20)
-        base.create_credts_and_debits(account=acc2, account_period=period, value=15)
+        base.create_debit_and_credit(
+            period=period, value=base.Decimal("50"), debit=debit, credit=credit
+        )
 
-        self.assertEqual(acc2.total_account_balance(), 0)
-        self.assertEqual(acc1.total_account_balance(), 0)
+        self.assertEqual(self.asset.total_account_balance(), 0)
 
     def test_total_account_balance_debit(self):
         period = base.create_period(self.user)
-        _, acc1, acc2 = base.create_children_accounts(self.user)
+        _, debit, _ = base.create_children_accounts(self.user, root=self.asset)
+        _, credit, _ = base.create_children_accounts(self.user, root=self.liability)
 
-        base.create_credts_and_debits(
-            account=acc1,
-            account_period=period,
-            value=20,
-            credit=False,
+        base.create_debit_and_credit(
+            period=period, value=base.Decimal("50"), debit=debit, credit=credit
         )
-        base.create_credts_and_debits(
-            account=acc2,
-            account_period=period,
-            value=15,
-            credit=False,
+        base.create_debit_and_credit(
+            period=period, value=base.Decimal("25"), debit=debit, credit=credit
         )
 
-        self.assertEqual(acc1.total_account_balance(), 35)
-        self.assertEqual(acc2.total_account_balance(), 15)
-
-    def test_total_account_balance_credit(self):
-        period = base.create_period(self.user)
-        _, acc1, acc2 = base.create_children_accounts(self.user)
-
-        base.create_credts_and_debits(
-            account=acc1,
-            account_period=period,
-            value=20,
-            debit=False,
-        )
-        base.create_credts_and_debits(
-            account=acc2,
-            account_period=period,
-            value=15,
-            debit=False,
-        )
-
-        self.assertEqual(acc1.total_account_balance(), -35)
-        self.assertEqual(acc2.total_account_balance(), -15)
+        self.assertEqual(self.asset.total_account_balance(), 75)
+        self.assertEqual(self.liability.total_account_balance(), 75)
