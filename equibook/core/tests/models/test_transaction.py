7aed9fd787a5821c9e27d6109ae6ebc650c71c4c
Pedro Belo <pn999717@gmail.com>
2024-01-18 09:51:30 -0300

    TransactionModelTestCase

--- equibook/core/tests/models/test_transaction.py
+++ equibook/core/tests/models/test_transaction.py
@@ -1,16 +1,19 @@
 from equibook.core.tests import base
+from equibook.core import facade
 
 
 class TransactionModelTestCase(base.TestCase):
     def setUp(self) -> None:
         self.user = base.create_default_user()
+        self.asset = facade.Account.objects.get_asset(self.user)
+        self.liability = facade.Account.objects.get_liability(self.user)
 
     def test_transaction_balance_zero(self):
         period = base.create_period(self.user)
-        _, acc1, _ = base.create_children_accounts(self.user)
+        _, debit, credit = base.create_children_accounts(self.user, self.asset)
 
-        transaction = base.create_credts_and_debits(
-            account=acc1, account_period=period, value=20, reperat=2
+        transaction = base.create_debit_and_credit(
+            period=period, value=40, debit=debit, credit=credit
         )
 
         balance = transaction.get_balance()
@@ -19,13 +22,12 @@ class TransactionModelTestCase(base.TestCase):
         self.assertEqual(balance["credit_sum"], 40)
         self.assertEqual(balance["is_balanced"], True)
 
-
     def test_transaction_balance_credit(self):
         period = base.create_period(self.user)
-        _, _, acc2 = base.create_children_accounts(self.user)
+        _, credit, _ = base.create_children_accounts(self.user, self.asset)
 
-        transaction = base.create_credts_and_debits(
-            account=acc2, account_period=period, value=20, reperat=2, debit=False
+        transaction = base.create_debit_and_credit(
+            period=period, value=40, credit=credit
         )
 
         balance = transaction.get_balance()
@@ -34,13 +36,11 @@ class TransactionModelTestCase(base.TestCase):
         self.assertEqual(balance["credit_sum"], 40)
         self.assertEqual(balance["is_balanced"], False)
 
-def test_transaction_balance_debit(self):
+    def test_transaction_balance_debit(self):
         period = base.create_period(self.user)
-        _, _, acc2 = base.create_children_accounts(self.user)
+        _, debit, _ = base.create_children_accounts(self.user)
 
-        transaction = base.create_credts_and_debits(
-            account=acc2, account_period=period, value=20, reperat=2, credit=False
-        )
+        transaction = base.create_debit_and_credit(period=period, value=40, debit=debit)
 
         balance = transaction.get_balance()
         self.assertEqual(balance["instance"].id, transaction.id)
