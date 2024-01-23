a02c18d2246e0319d61bf32222c0d58242069462
Pedro Belo <pn999717@gmail.com>
2024-01-22 21:01:41 -0300

    TrialBalanceTestCase

--- equibook/core/tests/facade/test_trial_balance.py
+++ equibook/core/tests/facade/test_trial_balance.py
@@ -1,9 +1,62 @@
 from equibook.core.tests import base
 from equibook.core import facade
 from equibook.core.facade.base import create_dict
-from equibook.core.facade.trial_balance import TrialBalanceItem
+from equibook.core.facade.trial_balance import TrialBalanceItem, TrialBalance
 from equibook.core.cache.account import cache_get_accounts
 
+
+class TrialBalanceTestCase(base.TestCase):
+    def setUp(self) -> None:
+        self.user = base.create_default_user()
+        self.period = base.create_period(self.user)
+
+        (
+            self.asset,
+            self.equity,
+            self.expense,
+            self.revenue,
+        ) = base.setup_accounting_scenario(self.user, self.period)
+
+        self.accounts_d = create_dict(cache_get_accounts(self.user))
+        self.trial_balance = TrialBalance(
+            transaction=facade.Transaction.objects.get(next=None)
+        )
+
+    def test_trial_balance_accounts_items(self):
+        result = self.trial_balance.get_result()
+        self.assertEqual(len(result["accounts"].keys()), 4)
+        self.assertIn(self.asset.id, result["accounts"])
+        self.assertIn(self.equity.id, result["accounts"])
+        self.assertIn(self.expense.id, result["accounts"])
+        self.assertIn(self.revenue.id, result["accounts"])        
+
+    def test_asset(self):
+        result = self.trial_balance.get_result()
+        asset = result["accounts"][self.asset.id]
+        self.assertEqual(asset.balance(), 1000)
+        self.assertEqual(asset.debit_sum(), 1800)
+        self.assertEqual(asset.credit_sum(), 800)
+
+    def test_equity(self):
+        result = self.trial_balance.get_result()
+        equity = result["accounts"][self.equity.id]
+        self.assertEqual(equity.balance(), 1000)
+        self.assertEqual(equity.debit_sum(), 0)
+        self.assertEqual(equity.credit_sum(), 1000)
+
+    def test_expense(self):
+        result = self.trial_balance.get_result()
+        expense = result["accounts"][self.expense.id]
+        self.assertEqual(expense.balance(), 500)
+        self.assertEqual(expense.debit_sum(), 700)
+        self.assertEqual(expense.credit_sum(), 200)
+
+    def test_revenue(self):
+        result = self.trial_balance.get_result()
+        revenue = result["accounts"][self.revenue.id]
+        self.assertEqual(revenue.balance(), 500)
+        self.assertEqual(revenue.debit_sum(), 100)
+        self.assertEqual(revenue.credit_sum(), 600)
 
 class TrialBalanceItemTestCase(base.TestCase):
     def setUp(self) -> None:
