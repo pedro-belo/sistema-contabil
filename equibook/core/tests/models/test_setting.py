5524ddb4b4f1037d75656d306998881b442675dc
Pedro Belo <pn999717@gmail.com>
2024-08-21 16:09:45 -0300

    fix: tests quebrados

--- equibook/core/tests/models/test_setting.py
+++ equibook/core/tests/models/test_setting.py
@@ -1,4 +1,3 @@
-from equibook.core import facade
 from equibook.core.tests import base
 
 
@@ -6,7 +5,7 @@ class AccountModelTestCase(base.TestCase):
     def setUp(self) -> None:
         self.user = base.create_default_user()
     
-    def test_to_dict_except_dark_mode(self):
+    def test_to_dict(self):
         user_settings = self.user.user_settings
         setting_d = user_settings.to_dict()
         self.assertEqual(setting_d["entity"], user_settings.entity)
@@ -16,19 +15,3 @@ class AccountModelTestCase(base.TestCase):
         self.assertEqual(setting_d["current_currency"]["label"], user_settings.get_current_currency_display())
         self.assertEqual(setting_d["exchange_rate"], user_settings.exchange_rate)
         self.assertEqual(setting_d["defined_first_period"], False)
-        self.assertEqual(setting_d["theme"]["code"], user_settings.theme)
-        self.assertEqual(setting_d["theme"]["label"], user_settings.get_theme_display())
-
-    def test_to_dict_dark_mode_light(self):
-        user_settings = self.user.user_settings
-        user_settings.theme = facade.Setting.Theme.LIGHT
-        user_settings.save()
-        setting_d = user_settings.to_dict()
-        self.assertEqual(setting_d["DARK_MODE"], False)
-
-    def test_to_dict_dark_mode_dark(self):
-        user_settings = self.user.user_settings
-        user_settings.theme = facade.Setting.Theme.DARK
-        user_settings.save()
-        setting_d = user_settings.to_dict()
-        self.assertEqual(setting_d["DARK_MODE"], True)
